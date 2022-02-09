72dd887ef90858ddda13f181c747836c47fde520
liuzixing-at-591536158659 <liuzixing@qq.com>
2022-02-09 03:15:05 +0000

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Cmd(object):
+    def contribute_cmd(self):
+        cmd = "pip install zipp"
+        return cmd
+    
+    def run_cmd(self,cmd):
+        print(cmd)
+        a = os.system(cmd)
+        print(a)
+        
+if __name__ == '__main__':
+    code = Cmd()
+    cmd = code.contribute_cmd()
+    code.run_cmd(cmd)
